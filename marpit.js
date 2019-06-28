6265e024cd0cbd8d14e0402beaf107ca4ef7c156
afsalthaj <afsal.taj06@gmail.com>
2019-06-28 14:55:11 +1000

    Add a spark execution detail

--- marpit.js
+++ marpit.js
@@ -1,27 +1,46 @@
 const { Marpit } = require('@marp-team/marpit')
 const marpit = new Marpit()
 
-marpit.themeSet.default = marpit.themeSet.add(`
-/* @theme my-first-theme */
+// 2. Add theme CSS
+const theme = `
+/* @theme example */
 
 section {
-  background-color: #123;
+  background-color: #369;
   color: #fff;
   font-size: 30px;
   padding: 40px;
 }
 
+h1,
+h2 {
+  text-align: center;
+  margin: 0;
+}
+
 h1 {
   color: #8cf;
 }
-`)
+`
+
+marpit.themeSet.default = marpit.themeSet.add(theme)
+
+const markdown = `
+
+# Hello, Marpit!
+
+Marpit is the skinny framework for creating slide deck from Markdown.
+
+---
+
+## Ready to convert into PDF!
+
+You can convert into PDF slide deck through Chrome.
+
+`
 
 module.exports = () => {  
-  return marpit.render(`
-    Spark Execution plans
-
-    `
-  )
+  return marpit.render(markdown)
 }
 
 
